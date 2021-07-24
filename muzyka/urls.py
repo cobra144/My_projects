605e7976126148d93c562a983b5fd838f5b6efef
cobra144 <dawidbedd@gmail.com>
2021-07-24 12:05:04 +0200

    include_html_added

--- muzyka/urls.py
+++ muzyka/urls.py
@@ -12,14 +12,12 @@ router.register('muzyka',Apiview,basename='api_muzyka')
 
 urlpatterns = [
                   path('', views.post_list, name='searchbar'),
-                  path('powitanie/', views.metadane, name="pierwsza"),
                   path('searchbar/', views.post_list, name="searchbar"),
                   path('searchbar_bez_user/', views.post_list_bez_user, name="searchbar_bez_user"),
                   path('foto/<slug:slug>', views.fotopage, name="fotopage"),
                   path('kategorie/', views.kategorie, name="kategorie"),
                   path('<slug:category_slug>', views.kategorie, name='story_by_category'),
                   path('foto/<slug:slug>', views.paginacja, name='paginacja'),
-                  path('pagination/', views.paginacja, name='pagination'),
                   path('register/', views.register, name='register'),
                   path('login/', auth_views.LoginView.as_view(template_name="login.html",), name="login"),
                   path('formularz/', views.get_question, name='formularz'),
